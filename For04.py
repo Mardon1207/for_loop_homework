ed27ac2272c84b2f4a3e852b59b316991c64129b
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-02 19:55:38 +0500

    4

--- For04.py
+++ For04.py
@@ -7,4 +7,10 @@ def main(A,B):
     Returns:
         list: return  answer
     """
-    return
+    l=[]
+    for i in range(A,B+1):
+        l.append(i)
+    return l
+A=int(input())
+B=int(input())
+print(main(A,B))
